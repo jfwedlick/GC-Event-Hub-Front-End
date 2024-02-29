2466efe63376d28d547de18f44e9aa89e31b0541
Peggy Sabido <peggy_sabido@hotmail.com>
2024-02-28 21:12:35 -0500

    FixingFavorites

--- src/app/Interfaces/event.ts
+++ src/app/Interfaces/event.ts
@@ -15,3 +15,11 @@ export interface SubmitEventInfo {
     duration: number
 }
 
+export interface UserEvents{
+    usrEvent: boolean;
+    eventId: number;
+    eventName: string;
+    favoriteId:number;
+    favorite: boolean;
+}
+
